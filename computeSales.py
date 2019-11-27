62381269a842325c7588f27d085c90083d721e18
Sotiris Kavvouras <Sotiris>
2019-11-27 18:24:38 +0200

    possible finish

--- computeSales.py
+++ computeSales.py
@@ -1,5 +1,6 @@
 receipts = {}
 
+
 def parse_into_list(file):
     res, temp = [],[]
     for l in file:
@@ -8,8 +9,19 @@ def parse_into_list(file):
             temp = []
         else:
             temp.append(l)
-    #print(res)
     return res
+
+
+def merge(receipts, afm, dictionary):
+    if afm in receipts.keys():
+        for key in dictionary.keys():
+            if key in receipts[afm].keys():
+                receipts[afm][key][0] += dictionary[key][0]
+                receipts[afm][key][1] += dictionary[key][1]
+            else:
+                receipts[afm][key] = dictionary[key]
+    else:
+        receipts[afm] = dictionary
 
 
 def readFile(filename):
@@ -23,23 +35,39 @@ def readFile(filename):
         temp = {}
         if 'ΑΦΜ' in receipt[0] and len(receipt[0].split()[1])==10 and 'ΣΥΝΟΛΟ' in receipt[-1]:
             total = 0
-            for i in range(1, len(receipt)-1):
-                product = receipt[i].split()
-                key = product[0].upper()
-                if round(int(product[1])*float(product[2]) == float(product[3])):
-                    if key in temp.keys():
-                        temp[key][0] += int(product[1])
-                        temp[key][2] += float(product[3])
+            try:
+                afm = int(receipt[0].split()[1])
+            except (NameError, ValueError):
+                afm = ''
+            if afm != '':
+                for i in range(1, len(receipt)-1):
+                    product = receipt[i].split()
+                    key = product[0].upper()[:-1]
+                    if round(int(product[1])*float(product[2]), 2) == float(product[3]) and key != '':
+                        if key in temp.keys():
+                            temp[key][0] += int(product[1])
+                            temp[key][1] += float(product[3])
+                        else:
+                            temp[key] = [int(product[1]), float(product[3])]
+                        total += float(product[3])
                     else:
-                        temp[key] = [int(product[1]), float(product[2]), float(product[3])]
-                    total += float(product[3])
-                    print(temp)
-                else:
-                    temp = {}
-                    total = 0
-                    break
-                
-        
+                        temp = {}
+                        total = 0
+                        break
+                    #print(temp)
+                if round(total, 2) == round(float(receipt[-1].split()[1]), 2):
+                    merge(receipts, afm, temp)
+
+
+def print_statistics_for_product(product):
+    for afm in sorted(receipts.keys()):
+        if product.upper() in receipts[afm].keys():
+            print(str(afm)+' '+'{0:.2f}'.format(round(receipts[afm][product.upper()][1],2)))
+
+
+def print_statistics_for_afm(afm):
+    for product in sorted(receipts[afm].keys()):
+        print(product+' '+'{0:.2f}'.format(round(receipts[afm][product.upper()][1],2)))
 
 
 while True:
@@ -54,9 +82,15 @@ while True:
         except FileNotFoundError:
             pass
     elif answer == 2:
-        pass
+        try:
+            print_statistics_for_product(input("Give preference:\t"))
+        except KeyError:
+            pass
     elif answer == 3:
-        pass
+        try:
+            print_statistics_for_afm(int(input("Give preference:\t")))
+        except KeyError:
+            pass
     elif answer == 4:
         break
     else:
