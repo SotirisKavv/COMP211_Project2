1df1e440270bc57e74b2d6a7250f917ebf97c403
Sotiris Kavvouras <Sotiris>
2019-11-27 14:38:27 +0200

    gradually read first receipts

--- computeSales.py
+++ computeSales.py
@@ -8,7 +8,7 @@ def parse_into_list(file):
             temp = []
         else:
             temp.append(l)
-    print(res)
+    #print(res)
     return res
 
 
@@ -17,7 +17,29 @@ def readFile(filename):
     file = open(filename, encoding='utf-8')
     file.readline()
     helper = parse_into_list(file)
-    print(helper)
+    #print(helper)
+
+    for receipt in helper:
+        temp = {}
+        if 'ΑΦΜ' in receipt[0] and len(receipt[0].split()[1])==10 and 'ΣΥΝΟΛΟ' in receipt[-1]:
+            total = 0
+            for i in range(1, len(receipt)-1):
+                product = receipt[i].split()
+                key = product[0].upper()
+                if round(int(product[1])*float(product[2]) == float(product[3])):
+                    if key in temp.keys():
+                        temp[key][0] += int(product[1])
+                        temp[key][2] += float(product[3])
+                    else:
+                        temp[key] = [int(product[1]), float(product[2]), float(product[3])]
+                    total += float(product[3])
+                    print(temp)
+                else:
+                    temp = {}
+                    total = 0
+                    break
+                
+        
 
 
 while True:
